3965034e541414d7a11c74e3eebbec3de0548076
Antony F. M <antonyfm@gmail.com>
2023-10-20 12:19:05 -0700

    Updated binomial_option_model.py to include deltaT and standard deviation

--- binomial_option_model.py
+++ binomial_option_model.py
@@ -1,36 +1,51 @@
 import numpy as np
+import math
 
-
-def binomial_model(N, S0, u, r, K):
+def binomial_model(N, T, S0, K, rf, SD):
     """
     N = number of binomial iterations
+    T = total time period
     S0 = initial stock price
-    u = factor change of upstate
-    r = risk free interest rate per annum
-    K = strike price
+    K = strike price    
+    rf = risk free interest rate per annum
+    SD = Standard deviation
+    
+    Calculated values
+    deltaT = Total time / No of steps
+    u = e^(SD * sqrt(deltaT))
+    d = 1/u
+    p = (e^(rf X delta t) - d) / (u - d)
+    q = 1 - p
     """
+    
+    deltaT = T / N
+    u = math.exp(SD * math.sqrt(deltaT))
     d = 1 / u
-    p = (1 + r - d) / (u - d)
+    p = (math.exp(rf * deltaT) - d) / (u - d)
     q = 1 - p
 
     # make stock price tree
     stock = np.zeros([N + 1, N + 1])
-    for i in range(N + 1):
+    for i in range((N + 1)):
         for j in range(i + 1):
             stock[j, i] = S0 * (u ** (i - j)) * (d ** j)
+    print("Forward binomial pricing tree:")
+    print (stock)
 
     # Generate option prices recursively
+    value = math.exp(-1 * rf * deltaT)
+    print(value)
     option = np.zeros([N + 1, N + 1])
     option[:, N] = np.maximum(np.zeros(N + 1), (stock[:, N] - K))
-    for i in range(N - 1, -1, -1):
+    for i in range(N - 1,-1,-1):
         for j in range(0, i + 1):
             option[j, i] = (
-                1 / (1 + r) * (p * option[j, i + 1] + q * option[j + 1, i + 1])
+                math.exp(-1 * rf * deltaT) * (p * option[j, i + 1] + q * option[j + 1, i + 1])
             )
-    return option
-
+    print("Reverse binomial pricing tree:")
+    print(option)
+    return 0
 
 if __name__ == "__main__":
     print("Calculating example option price:")
-    op_price = binomial_model(5, 4, 2, 0.25, 8)
-    print(op_price)
+    op_price = binomial_model(3, 0.25, 2, 2.1, 0.08, 0.63)
